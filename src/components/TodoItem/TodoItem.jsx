1d8e73708a60bd5fef251a4e34007b677fc3bdf2
Ms-KL-Codes <thekristyleigh@gmail.com>
2023-02-12 08:21:46 +0800

    added undo/complete button toggle

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -10,10 +10,13 @@ function ToDoItem(props) {
         // can we just have the todo object
     // like: from ToDoItem import todo as props
     return (
-        <div className={`todo ${todo.isCompleted ? "complete": ""}`}>
+        <div className={`todo ${todo.isComplete ? "complete": ""}`}>
+        {/* if the todo = complete, class name is complete, otherwise there is no classname */}
         {todo.text}
         <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => completeTodo(index)}>
+            {todo.isComplete ? "Undo" : "Complete"}
+            </button>
         <button onClick={() => removeTodo(index)}>x</button>
         <button onClick={() => moveUp(index)}>↑</button>
         <button onClick={() => moveDown(index)}>↓</button>
